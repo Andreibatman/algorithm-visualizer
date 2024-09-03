422faa2a55368391abcf159b60d731dfa96ad668
Croitoriu Andrei Bogdan <oficialandreicroitoriu@yahoo.com>
2024-09-04 01:42:28 +0300

    Control speed + new design

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const sourceBtn = document.getElementById("source-btn");
 const destinationBtn = document.getElementById("destination-btn");
 const obstacleBtn = document.getElementById("obstacle-btn");
 const runBtn = document.getElementById("run-btn");
+const slider = document.getElementById("grid-speed");
 
+let stopPrevious = 0;
 let gridModifier = '';
 let rows,columns;
 let gridMatrix = [],solveMatrix = [];
@@ -16,9 +18,15 @@ let sourceArray = [];
 let destinationArray=[];
 let directionX = [-1,0,1,0];
 let directionY = [0,1,0,-1];
+let updateGridVisuals;
+let gridSpeed=2500;
+let countOfElements;
 
 const gridGenerator = (e) =>{
     event.preventDefault();
+    stopPrevious = 1;
+    clearTimeout(updateGridVisuals);
+    updateGridVisuals = 0;
     rows = Number(rowsCount.value);
     columns = Number(columnsCount.value);
     grid.innerHTML = ``;
@@ -29,6 +37,7 @@ const gridGenerator = (e) =>{
     const elementSize = Math.min(rowGridSize,columnGridSize);
     grid.style.gridAutoRows=`${elementSize}px`;
     grid.style.gridAutoColumns=`${elementSize}px`;
+    grid.style.fontSize =`${elementSize*2/3}px`;
     console.log(elementSize);
     let i,j;
     for(i = 1; i <= rows; i++){
@@ -50,7 +59,7 @@ const gridGenerator = (e) =>{
     runBtn.hidden = false;
     obstacleBtn.hidden = false;
     destinationBtn.hidden = false;
-    
+    slider.hidden=false;
 }
 
 const gridButtonClick = (event) =>{
@@ -74,6 +83,12 @@ formId.addEventListener("submit",(event) => gridGenerator());
 const isInside = element => element[0]>=1 && element[0]<=rows && element[1]>=1 && element[1]<=columns;
 
 const updateVisual = element =>{
+    if(stopPrevious === 1){
+        clearTimeout(updateGridVisuals);
+        updateGridVisuals=0;
+        return;
+    }
+    console.log("Stop prev: ",stopPrevious);
     let currentRow = element[0];
     let currentColumn = element[1];
     const currentSpot = document.getElementById(`grid-${currentRow}-${currentColumn}`);
@@ -84,17 +99,31 @@ const updateVisual = element =>{
 }
 
 const solveBfs = () =>{
+    countOfElements = 1;
     while(sourceArray.length){
+        if(stopPrevious === 1){
+            clearTimeout(updateGridVisuals);
+            updateGridVisuals=0;
+            break;
+        }
         const el = sourceArray.shift();
         console.log(el);
         console.log(rows,columns);
         for(let direction = 0; direction < 5; direction++){
             let newEl = [el[0]+directionX[direction],el[1]+directionY[direction]];
-            
-            if(isInside(newEl) && solveMatrix[newEl[0]][newEl[1]]>solveMatrix[el[0]][el[1]]+1){
+            if(stopPrevious === 1){
+                clearTimeout(updateGridVisuals);
+                updateGridVisuals=0;
+                break;
+            }
+            if(isInside(newEl) && gridMatrix[newEl[0]][newEl[1]]!=="obstacle" && solveMatrix[newEl[0]][newEl[1]]>solveMatrix[el[0]][el[1]]+1){
                 console.log(newEl);
+                countOfElements++;
                 solveMatrix[newEl[0]][newEl[1]]=solveMatrix[el[0]][el[1]]+1;
-                setTimeout(() => {updateVisual(newEl)},solveMatrix[newEl[0]][newEl[1]]*500);
+                updateGridVisuals = setTimeout(() => {   
+                    
+                    updateVisual(newEl)
+                },countOfElements*gridSpeed);
                 sourceArray.push(newEl);
             }
         }
@@ -102,13 +131,19 @@ const solveBfs = () =>{
 }
 
 runBtn.addEventListener("click",()=>{
+    stopPrevious=1;
+    if(stopPrevious === 1){
+        console.log("oprire");
+        clearTimeout(updateGridVisuals);
+        updateGridVisuals=0;
+    }
     sourceArray=[];
     destinationArray=[];
     for(let i=1;i<=rows;i++)
         for(let j=1;j<=columns;j++){
             if(gridMatrix[i][j]==="source"){
                 sourceArray.push([i,j]);
-                solveMatrix[i][j]=1;
+                solveMatrix[i][j]=0;
             }
             else{
                 if(gridMatrix[i][j]==="destination")
@@ -116,6 +151,16 @@ runBtn.addEventListener("click",()=>{
                 solveMatrix[i][j]=rows*columns+1;
             }
     }
+    if(sourceArray.length === 0 || destinationArray.length === 0){
+        alert("You should select where to place your source elements and your destination elements!");
+        return;
+    }
     console.log("wow");
+    stopPrevious=0;
     solveBfs();
 });
+
+
+slider.addEventListener("change",() => {
+    gridSpeed = 5001-slider.value;
+})
